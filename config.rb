e6538e1b03fbc32d46d5eada15de6bcc72346ed0
Sinetheta <k.attfield@gmail.com>
2016-02-13 15:02:47 -0800

    Add homepage

--- config.rb
+++ config.rb
@@ -32,6 +32,8 @@ Dotenv.load
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+activate :directory_indexes
+
 ###
 # Helpers
 ###
